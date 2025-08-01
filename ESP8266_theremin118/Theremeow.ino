bed4e6ffa541d4d8999fe1bd1d185ec93c2504e8
Doc Pixel <leo.podechard@gmail.com>
2025-08-01 09:04:29 +0200

    Update and rename ESP8266_theremin118.ino to Theremeow.ino
    Optimisation du code
    Pulsation des yeux

--- ESP8266_theremin118/Theremeow.ino
+++ ESP8266_theremin118/Theremeow.ino
@@ -1,50 +1,59 @@
+//   |  _ \  ___   ___  |  _ \(_)_  _____| |
+//   | | | |/ _ \ / __| | |_) | \ \/ / _ \ |
+//   | |_| | (_) | (__  |  __/| |>  <  __/ |
+//   |____/ \___/ \___| |_|   |_/_/\_\___|_|
+
+// > [ https://doc-pixel.com ]
+
 /////////////////////////////////////////////
 // LIBRARY
 
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
 
 #define SerialMon Serial
 #include <AppleMIDI_Debug.h>
+
 #include <AppleMIDI.h>
 
-/////////////////////////////////////////////
-// VARIABLES
-
 // Wifi Setup
-char ssid[] = "your_wifi_network"; //  your network SSID (name)
-char pass[] = "network_password";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "network"; //  your network SSID (name)
+char pass[] = "password";    // your network password (use for WPA, or use as key for WEP)
+
 unsigned long t0 = millis();
-int8_t isConnected = 0; //Used to check if the board is connected
-char deviceName[] = "thereminou";
+int8_t isConnected = 0;
+
+char deviceName[] = "theremineow";
 
 APPLEMIDI_CREATE_INSTANCE(WiFiUDP, MIDI, deviceName, 5004);
 
-
-// Ultrasonic Distance Sensor
-const int echoPin = 5;
-const int trigPin = 16;
-const int echoPin2 = 0;
-const int trigPin2 = 4;
+// PIN Ultrasonic Distance Sensor
+const int echoPin = 23;
+const int trigPin = 22;
+const int echoPin2 = 18;
+const int trigPin2 = 19;
 const int triggerMaxDistance = 50; // Read info under 50cm
 
-// Control MIDI
-int controlMidiNumber = 31; // Number of the controler you want to use
-int controlMidiNumber2 = 32;
+// PIN LED for the eyes
+const int eyes = 21;
 
+// Control MIDI
+int controlMidiNumber = 31;
+int controlMidiNumber2 = 32;
 
 /////////////////////////////////////////////
 // FUNCTION
 
 // Calculate the Ultrasonic Distance Sensor (SR04) and return a value between 0 and 127
-float readSensorDataControl(){
-  digitalWrite(trigPin, LOW); 
+
+float readSensorDataControl(int trigPinSensor, int echoPinSensor){
+  digitalWrite(trigPinSensor, LOW); 
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); 
+  digitalWrite(trigPinSensor, HIGH); 
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);  
-  int distance = pulseIn(echoPin, HIGH)/58;  //58 Equivalent to (340m/s*1us)/2
+  digitalWrite(trigPinSensor, LOW);  
+  int distance = pulseIn(echoPinSensor, HIGH)/58;  //58 Equivalent to (340m/s*1us)/2
   if (distance <= triggerMaxDistance){
     int correctedInput = map(distance, 0, triggerMaxDistance, 127, 0); // Map 0 - 50cm to 0 - 127 Control Change
     return correctedInput;
@@ -53,21 +62,7 @@ float readSensorDataControl(){
   }
 }
 
-// Calculate the Ultrasonic Distance Sensor (SR04) and return a value between 0 and 127
-float readSensorDataControl2(){
-  digitalWrite(trigPin2, LOW); 
-  delayMicroseconds(2);
-  digitalWrite(trigPin2, HIGH); 
-  delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW);  
-  int distance = pulseIn(echoPin2, HIGH)/58;  //58 Equivalent to (340m/s*1us)/2
-  if (distance <= triggerMaxDistance){
-    int correctedInput = map(distance, 0, triggerMaxDistance, 127, 0);
-    return correctedInput;
-  } else {
-    return 0;
-  }
-}
+
 
 // -----------------------------------------------------------------------------
 //
@@ -79,12 +74,9 @@ void setup()
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin2, INPUT);
   pinMode(trigPin2, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(eyes, OUTPUT);
 
-  // Serial Monitor Debug
   DBG_SETUP(115200);
-
-  // Start Wifi
   DBG("Booting");
 
   WiFi.begin(ssid, pass);
@@ -94,14 +86,12 @@ void setup()
     DBG("Establishing connection to WiFi..");
   }
   DBG("Connected to network");
+
   DBG(F("OK, now make sure you an rtpMIDI session that is Enabled"));
-  DBG(F("Add device named Arduino with Host"), WiFi.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
-  DBG(WiFi.macAddress()); //Important if you want to configure a fixed IP adress
-  DBG(F("Select and then press the Connect button"));
-  DBG(F("Then open a MIDI listener and monitor incoming notes"));
+  DBG(F("Add device with Host"), WiFi.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
+  DBG(WiFi.macAddress());
   DBG(F("Listen to incoming MIDI commands"));
 
-  // MIDI
   MIDI.begin();
 
   AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc, const char* name) {
@@ -120,6 +110,7 @@ void setup()
     DBG(F("NoteOff"), note);
   });
 
+  DBG(F("Thereminou is ready to work"));
 }
 
 // -----------------------------------------------------------------------------
@@ -129,27 +120,37 @@ void loop()
 {
   // Listen to incoming notes
   MIDI.read();
-  
-  
+
+  int fadeValue = 0;
+
+  // send Midi CC
   if (isConnected > 0)
   {
-
-    // LED on to tell  that the connection is OK !
-    digitalWrite(LED_BUILTIN, HIGH);
-
     // Send CC Midi Message _ CUTOFF
-    unsigned int ccValue = readSensorDataControl();
+    unsigned int ccValue = readSensorDataControl(trigPin, echoPin);
+    unsigned int prevccValue = 0;
+
     if ((ccValue) > 0 && (ccValue <= 127)) {
       MIDI.sendControlChange(controlMidiNumber, ccValue, 1); // control number, value, channel
+      fadeValue = map(ccValue, 0, 127, 0, 255); // Map 0 - 127 Control Change to 0 - 255 LED intensity
+      analogWrite(eyes, fadeValue);
+      prevccValue = ccValue;
     }
 
     // Send CC Midi Message _ RESO
-    unsigned int ccValue2 = readSensorDataControl2();
+    unsigned int ccValue2 = readSensorDataControl(trigPin2, echoPin2);
     if ((ccValue2) > 0 && (ccValue2 <= 127)) {
       MIDI.sendControlChange(controlMidiNumber2, ccValue2, 1); // control number, value, channel
     }
 
-  delay(50); //debounce
-  }
+    // If nothing happen during 5 sec : make the eyes bright at the maximum
+    if ((ccValue == prevccValue) && ((millis() - t0) > 5000)) {
+      t0 = millis();
+      analogWrite(eyes, 255);
+    }
 
+    delay(10); //debounce
+
+    
+  }
 }
